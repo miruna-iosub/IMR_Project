5f331ac89f80eb8c0b91f1461be535cf59d207b1
VornicuCatalina <100404375+VornicuCatalina@users.noreply.github.com>
2023-12-07 17:10:32 +0200

    Character movement & set-up

---
+++ Muzeul de Arta Craiova - Digital Twin/Assets/CharacterMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Animator mAnimator;
+    public GameObject egg;
+    bool checker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = egg.GetComponent<Animator>();
+        checker = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mAnimator != null)
+        {
+            if (!checker)
+            {
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+                {
+                    mAnimator.SetTrigger("WalkingTrig");
+                    print("Walks");
+                    checker = true;
+                }
+            }
+            else
+            {
+                if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+                {
+                    mAnimator.SetTrigger("IdleTrig");
+                    print("Stops");
+                    checker = false;
+                }
+            }
+
+        }
+    }
+}
